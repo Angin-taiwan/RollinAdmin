4999f7fd24ef6c8b51e866d2165b27ffbae60f10
Porter <porterbridgesds@gmailcom>
2020-02-26 16:37:35 +0800

    add Cart.sql

---
+++ setupDB/Cart.sql
@@ -0,0 +1,25 @@
+--
+-- Create database "Rollin" if not exists
+--
+
+CREATE DATABASE IF NOT EXISTS Rollin DEFAULT character set utf8;
+
+use Rollin;
+
+--
+-- Table structure for table `Cart`
+--
+
+DROP TABLE IF EXISTS `Cart`;
+
+CREATE TABLE `Cart`(
+   UserID       INT 	NOT NULL,
+   ProductID    INT 	NOT NULL,
+   SizeID       INT 	NOT NULL,
+   ColorID      INT 	NOT NULL,
+   CartQuantity INT 	NOT NULL,
+   PRIMARY KEY ( UserID, ProductID, SizeID, ColorID, CartQuantity )
+);
+
+
+--- TODO ... FK

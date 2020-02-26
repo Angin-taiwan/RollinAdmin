2adfa1169ea8073c46460e833147d3b0df0ccc3b
Porter <porterbridgesds@gmailcom>
2020-02-26 16:52:10 +0800

    add DiscountType.sql

---
+++ setupDB/DiscountType.sql
@@ -0,0 +1,19 @@
+  --
+-- Create database "Rollin" if not exists
+--
+
+CREATE DATABASE IF NOT EXISTS Rollin DEFAULT character set utf8;
+
+use Rollin;
+
+--
+-- Table structure for table `DiscountType`
+--
+
+DROP TABLE IF EXISTS `DiscountType`;
+
+CREATE TABLE `DiscountType`(
+   DiscountTypeID  	INT     	    NOT NULL  AUTO_INCREMENT,
+   DiscountName     VARCHAR(20)   NOT NULL,
+   PRIMARY KEY ( DiscountTypeID )
+);

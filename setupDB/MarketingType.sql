edf340398127a364854b581ba3fcfea587d8a313
Porter <porterbridgesds@gmailcom>
2020-02-26 16:46:30 +0800

    add MarketingType.sql

---
+++ setupDB/MarketingType.sql
@@ -0,0 +1,19 @@
+--
+-- Create database "Rollin" if not exists
+--
+
+CREATE DATABASE IF NOT EXISTS Rollin DEFAULT character set utf8;
+
+use Rollin;
+
+--
+-- Table structure for table `MarketingType`
+--
+
+DROP TABLE IF EXISTS `MarketingType`;
+
+CREATE TABLE `MarketingType`(
+   MarketingTypeID  	INT     	  NOT NULL  AUTO_INCREMENT,
+   MarketingTypeName    VARCHAR(20)   NOT NULL,
+   PRIMARY KEY ( MarketingTypeID )
+);

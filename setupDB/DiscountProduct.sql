adfa9468d9279bb95fb43d592fa22a5eee3a3b90
Porter <porterbridgesds@gmailcom>
2020-02-26 16:55:23 +0800

    add DiscountProduct.sql

---
+++ setupDB/DiscountProduct.sql
@@ -0,0 +1,19 @@
+  --
+-- Create database "Rollin" if not exists
+--
+
+CREATE DATABASE IF NOT EXISTS Rollin DEFAULT character set utf8;
+
+use Rollin;
+
+--
+-- Table structure for table `DiscountProduct`
+--
+
+DROP TABLE IF EXISTS `DiscountProduct`;
+
+CREATE TABLE `DiscountProduct`(
+   DiscountGroupID  	INT     NOT NULL  AUTO_INCREMENT,
+   ProductID            INT  	NOT NULL,
+   PRIMARY KEY ( DiscountGroupID, ProductID )
+);

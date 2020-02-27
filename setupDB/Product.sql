445ec611bcefe7938db05aeb192fafab8d9f1308
Angin-taiwan <Angin-taiwan@users.noreply.github.com>
2020-02-27 11:28:58 +0800

    02271128 BD BY AGC
    修改部分DataBases

--- setupDB/Product.sql
+++ setupDB/Product.sql
@@ -1,11 +1,36 @@
-CREATE TABLE `rollin`.`product` ( 
-    `ProductID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT , 
+-- made by AGC
+
+--
+-- Create database "Rollin" if not exists
+--
+
+CREATE DATABASE IF NOT EXISTS rollin DEFAULT character set utf8;
+
+use Rollin;
+
+--
+-- Table structure for table `Product`
+--
+
+DROP TABLE IF EXISTS `Product`;
+
+CREATE TABLE `Rollin`.`Product` ( 
+    `ProductID` INT(10) NOT NULL AUTO_INCREMENT , 
     `ProductName` VARCHAR(20) NOT NULL , 
     `BrandID` INT(10) NOT NULL , 
     `CategoryID` INT(10) NOT NULL , 
     `Description` TEXT NOT NULL , 
-    `Discontinued` VARCHAR(1) BINARY NOT NULL DEFAULT '0' , 
+    `Discontinued` VARCHAR(1) BINARY NOT NULLDEFAULT '0' COMMENT '預設0為販售中,1:停售' , 
     `UnitPrice` INT(20) NOT NULL , 
     `Date` VARCHAR(20) NOT NULL DEFAULT CURRENT_TIMESTAMP, 
     PRIMARY KEY (`ProductID`)
-    ) ENGINE = InnoDB;
+    --   ,CONSTRAINT `FK_Product_Brand` FOREIGN KEY (`BrandID`) REFERENCES `Rollin`.`Brand` (`BrandID`),
+    --   CONSTRAINT `FK_Product_Category` FOREIGN KEY (`CategoryID`) REFERENCES `Rollin`.`Category` (`CategoryID`)
+      ) ENGINE = InnoDB;
+
+INSERT INTO `product` (`ProductID`, `ProductName`, `BrandID`, `CategoryID`, `Description`, `Discontinued`, `UnitPrice`, `Date`) 
+    VALUES (NULL, 'Fashion Board', '1', '1', 'Skate really fast!!', '0', '3000', 'current_timestamp()'),
+           (NULL, 'Biggy Bag', '2', '21', 'Reeeeeally Big man!', '0', '2580', 'current_timestamp()')
+--
+-- Dumping data for table `product`
+--

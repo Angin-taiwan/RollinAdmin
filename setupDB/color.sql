39c7aa6eecb52454d178b61f20181858ceae4b41
Angin-taiwan <Angin-taiwan@users.noreply.github.com>
2020-02-27 11:33:28 +0800

    Revert "02271128 BD BY AGC"
    This reverts commit 445ec611bcefe7938db05aeb192fafab8d9f1308.

--- setupDB/color.sql
+++ setupDB/color.sql
@@ -1,25 +1,5 @@
--- made by AGC
-
---
--- Create database "Rollin" if not exists
---
-
-CREATE DATABASE IF NOT EXISTS rollin DEFAULT character set utf8;
-
-use Rollin;
-
---
--- Table structure for table `color`
---
-
-DROP TABLE IF EXISTS `color`;
-
 CREATE TABLE `rollin`.`color` ( 
     `ColorID` INT(10) NOT NULL AUTO_INCREMENT , 
     `Color` VARCHAR(20) NOT NULL , 
     PRIMARY KEY (`ColorID`)
     ) ENGINE = InnoDB;
-
---
--- Dumping data for table `color`
---
--- setupDB/size.sql
+++ setupDB/size.sql
@@ -1,26 +1,6 @@
--- made by AGC
-
---
--- Create database "Rollin" if not exists
---
-
-CREATE DATABASE IF NOT EXISTS rollin DEFAULT character set utf8;
-
-use Rollin;
-
---
--- Table structure for table `size`
---
-
-DROP TABLE IF EXISTS `size`;
-
 CREATE TABLE `rollin`.`size` ( 
     `SizeID` INT(10) NOT NULL AUTO_INCREMENT ,
     `CategoryID` INT(10) NOT NULL , 
     `SizeName` VARCHAR(20) NOT NULL , 
     PRIMARY KEY (`SizeID`)
     ) ENGINE = InnoDB;
-
---
--- Dumping data for table `size`
---

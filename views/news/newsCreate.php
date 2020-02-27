7c7bccfdd1631442be10cfc1158a5b0251b842b9
王韋竣 <leot7495@gmail.com>
2020-02-28 01:20:21 +0800

    Newslist

--- views/news/newsCreate.php
+++ views/news/newsCreate.php
@@ -3,17 +3,62 @@
 $pageDir = "News";
 $pageTitle = "News Create";
 
+
+
 require_once 'views/template/header.php';
+
+if (isset($_POST["insertButton"])) {
+  $news = new News();
+  $news->Title = $_POST["Title"];
+  $news->Description = $_POST["Description"];
+  $news->CreateDate = $_POST["CreateDate"];
+  $news->UpdateDate = $_POST["UpdateDate"];
+  // $commandText = 
+  //     "insert into News "
+  //   . "set Title = '{$Title}' "
+  //   . "  , Description       = '{$Description}'"
+  //   . "  , CreateDate    = '{$CreateDate}'"
+  //   . "  , UpdateDate    = '{$UpdateDate}'";
+  $data->create($news);
+}
 
 ?>
 
 <div class="container-fluid">
-  <?= $pageTitle ?> content here
+
+  <form method="post" action="News/Create">
+
+    <div class="form-group">
+      <label for="Title" class="text">
+        Title :
+      </label>
+      <input type="text" class="form-control" name="Title" id="Title">
+    </div>
+
+    <div class="form-group">
+      <label for="Description" class="text">
+        Description :
+      </label>
+      <input type="text" class="form-control" name="Description" id="Description" style="height:200px">
+    </div>
+
+    <div class="form-group">
+      <label for="CreateDate" class="date">
+        CreateDate :
+      </label>
+      <input type="date" class="form-control" name="CreateDate" id="CreateDate">
+    </div>
+
+    <div class="form-group">
+      <label for="UpdateDate" class="date">
+        UpdateDate :
+      </label>
+      <input type="date" class="form-control" name="UpdateDate" id="UpdateDate">
+    </div>
+    <input name="action" type="hidden" value="id">
+    <button type="submit" class="btn btn-success" name="insertButton" id="insertButton">確認送出</button>
+    <button type="reset" class="btn btn-danger">清除資料</button>
+
+  </form>
 </div>
 <!-- /.container-fluid -->
-
-<?php
-
-require_once 'views/template/footer.php';
-
-?>

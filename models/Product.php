f297a29c75e0dd2089a7307397345e73e697c23f
Angin-taiwan <Angin-taiwan@users.noreply.github.com>
2020-02-27 14:20:14 +0800

    0227 1418
    product新增基礎 models contro view,
    持續修改中~

--- models/Product.php
+++ models/Product.php
@@ -1,7 +1,25 @@
 <?php
 
-class Product {
-  public $id;
-}
+class Product extends DB {
 
-?>
+  function getAll(){
+    return $this->select("SELECT * FROM Product");
+  }
+
+  function get($id) {
+    return $this->select("SELECT * FROM Brand WHERE BrandID = ? ", [$id])[0];
+  }
+
+  function create() {
+
+  }
+
+  function update($id) {
+
+  }
+
+  function delete($id) {
+
+  }
+
+}
--- views/Product/productList.php
+++ views/Product/productList.php
@@ -5,12 +5,50 @@ $pageTitle = "Product List";
 
 require_once 'views/template/header.php';
 
+$products = $data->getAll();
+
 ?>
 
-<div class="container-fluid">
+<!-- <div class="container-fluid">
   <?= $pageTitle ?> content here
+</div> -->
+<!-- /.container-fluid -->
+<div class="container-fluid">
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Products</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+      <table id="listTable" class="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>ProductID</th>
+            <th>ProductName</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          foreach ($products as $pd) {
+            echo "<tr onclick=\"window.location='/RollinAdmin/Brand/Detail/" . $pd->ProductID . "'\">";
+            echo "<td> $pd->ProductID </td>";
+            echo "<td>$pd->ProductName</td>";
+            echo "<td>$pd->Description</td>";
+            echo "</tr>";
+          }
+          ?>
+        </tbody>
+        <tfoot>
+        </tfoot>
+      </table>
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
 </div>
 <!-- /.container-fluid -->
+
 
 <?php
 

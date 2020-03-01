e6a0c4b82fa02ca0ce3d05e541b76b8fc0fb49db
Porter <PorterBridgesDS+github@gmail.com>
2020-03-01 14:45:31 +0800

    fix sql 語法

--- models/Order.php
+++ models/Order.php
@@ -7,23 +7,24 @@ class Order extends DB {
   public $Description;
 
   function getAll() {
-    return $this->selectDB("SELECT * FROM Order ;");
+    return $this->selectDB("SELECT * FROM `Order` ;");
+    // return $this->selectDB("SELECT * FROM Order ;");
   }
 
   function get($id) {
-    return $this->selectDB("SELECT * FROM Order WHERE OrderID = ? ;", [$id])[0];
+    return $this->selectDB("SELECT * FROM `Order` WHERE OrderID = ? ;", [$id])[0];
   }
 
   function create($Order) {
     return $this->insertDB(
-      "INSERT INTO Order (OrderName, Description) VALUES (?, ?) ;",
+      "INSERT INTO `Order` (OrderName, Description) VALUES (?, ?) ;",
       [$Order->OrderName, "$Order->Description"]
     );
   }
 
   function update($Order) {
     return $this->updateDB(
-      "UPDATE Order SET OrderName = ?, Description = ? WHERE OrderID = ? ;",
+      "UPDATE `Order` SET OrderName = ?, Description = ? WHERE OrderID = ? ;",
       ["$Order->OrderName", "$Order->Description", $Order->OrderID]
     );
   }
@@ -31,7 +32,7 @@ class Order extends DB {
   function delete($ids = []) {
     if (empty($ids)) {return "error: ids is empty";}
     return $this->deleteDB(
-      "DELETE FROM Order WHERE OrderID IN (" . str_repeat("?,", count($ids) - 1) . "?);",
+      "DELETE FROM `Order` WHERE OrderID IN (" . str_repeat("?,", count($ids) - 1) . "?);",
       $ids);
   }
 

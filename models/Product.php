125c8d7260e3a1285d5a90ecf840003452cecb98
Angin-taiwan <Angin-taiwan@users.noreply.github.com>
2020-03-02 21:43:46 +0800

    20200302 AGC
    新增 Color Size 資料庫
    新增 Product View 下拉式選單顯示庫存

--- models/Product.php
+++ models/Product.php
@@ -2,29 +2,54 @@
 
 class Product extends DB {
 
+  public $ProductID ;
+  public $ProductName ;
+  public $BrandID ;
+  public $CategoryID ;
+  public $PDescription ;
+  public $Discontinued ;
+  public $UnitPrice ;
+  public $Date ;
+  public $UnitInStock;
+
+
   // $BrandID_Name = [];
 
   function getAll(){
     return $this->selectDB(
       "select * from product as p 
+          left outer join brand as b on p.BrandID = b.BrandID               -- 品牌
+          left outer join category as c on p.CategoryID = c.CategoryID      -- 類別
+          left outer join productstock as ps on p.productID = ps.productID  -- 庫存
+          ;"
+    );
+  }
+  function getStock($id){
+    return $this->selectDB(
+      "select * from product as p 
           left outer join brand as b on p.BrandID = b.BrandID 
-          left outer join category as c on p.CategoryID = c.CategoryID ;"
+          left outer join category as c on p.CategoryID = c.CategoryID 
+          left outer join productstock as ps on p.productID = ps.productID 
+          WHERE p.ProductID = ? ", [$id]
     );
   }
 
+
   function get($id) {
-    return $this->selectDB("SELECT * FROM Product WHERE ProductID = ? ", [$id])[0];
+    return $this->selectDB("SELECT * FROM Product WHERE ProductID = ? ", [$id]
+    )[0];
   }
 
-  function getBrand(){
-    return $this->selectDB("SELECT * FROM Brand ;"); 
+  function createProduct($Product) {
+    return $this->insertDB(
+      "INSERT INTO `product` (`ProductName`, `BrandID`, `CategoryID`, `PDescription`, `Discontinued`, 
+      `UnitPrice`) VALUES (?,?,?,?,?,?) ;", 
+      ["$Product->ProductName" , "$Product->BrandID" , $Product->CategoryID , $Product->PDescription , 
+      "$Product->Discontinued" , "$Product->UnitPrice" ]
+    );
   }
 
-  function create() {
-
-  }
-
-  function update($id) {
+  function updateProduct($Product) {
 
   }
 
--- setupDB/size.sql
+++ setupDB/size.sql
@@ -1,4 +1,4 @@
--- made by AGC
+-- made by AGC 2020-03-02
 
 --
 -- Create database "Rollin" if not exists
@@ -20,6 +20,21 @@ CREATE TABLE `rollin`.`size` (
     `SizeName` VARCHAR(20) NOT NULL , 
     PRIMARY KEY (`SizeID`)
     ) ENGINE = InnoDB;
+
+-- 板身CategoryID = '5' ; 6.5 ~ 10.25 O=One Size
+    INSERT INTO `size` (`SizeID`, `CategoryID`, `SizeName`) VALUES (NULL, '5', '6.5\"'), (NULL, '5', '6.75\"'), (NULL, '5', '7\"'), (NULL, '5', '7.25\"'), (NULL, '5', '7.375'), (NULL, '5', '7.5\"'), (NULL, '5', '7.625\"'), (NULL, '5', '7.75\"'), (NULL, '5', '7.875\"'), (NULL, '5', '8\"'), (NULL, '5', '8.125\"'), (NULL, '5', '8.25\"'), (NULL, '5', '8.375\"'), (NULL, '5', '8.5\"'), (NULL, '5', '8.625\"'), (NULL, '5', '8.75\"'), (NULL, '5', '8.875\"'), (NULL, '5', '9\"'), (NULL, '5', '9.25\"'), (NULL, '5', '9.5'), (NULL, '5', '10\"'), (NULL, '5', '10.25\"'), (NULL, '5', 'O') ;
+    
+ -- 鞋子 CategoryID = '2' ; US 7- ~ US 13+
+    INSERT INTO `size` (`SizeID`, `CategoryID`, `SizeName`) VALUES (NULL, '2', 'US 7-'), (NULL, '2', 'US 7'), (NULL, '2', 'US 7.5'), (NULL, '2', 'US 8'), (NULL, '2', 'US 8.5'), (NULL, '2', 'US 9'), (NULL, '2', 'US 9.5'), (NULL, '2', 'US 10'), (NULL, '2', 'US 10.5'), (NULL, '2', 'US 11'), (NULL, '2', 'US 11.5'), (NULL, '2', 'US 12'), (NULL, '2', 'US 12.5'), (NULL, '2', 'US 13'), (NULL, '2', 'US 13+') ;
+         
+-- 衣服CategoryID = '3' 
+    -- 上身 xxS ~ xxL 
+        INSERT INTO `size` (`SizeID`, `CategoryID`, `SizeName`) VALUES (NULL, '3', 'XXS'), (NULL, '3', 'XS'), (NULL, '3', 'S'), (NULL, '3', 'M'), (NULL, '3', 'L'), (NULL, '3', 'XL'), (NULL, '3', 'XXL') ;
+    -- 褲子 W24 ~ W38
+        INSERT INTO `size` (`SizeID`, `CategoryID`, `SizeName`) VALUES (NULL, '3', 'W24'), (NULL, '3', 'W25'), (NULL, '3', 'W26'), (NULL, '3', 'W27'), (NULL, '3', 'W28'), (NULL, '3', 'W29'), (NULL, '3', 'W30'), (NULL, '3', 'W31'), (NULL, '3', 'W32'), (NULL, '3', 'W33'), (NULL, '3', 'W34'), (NULL, '3', 'W35'), (NULL, '3', 'W36'), (NULL, '3', 'W37'), (NULL, '3', 'W38') ;
+
+
+        
 
 --
 -- Dumping data for table `size`

4fe5e32172f8867f3a067943dfab87ded37f34ff
tzuchihsiao <judy112825@gmail.com>
2020-03-03 14:03:14 +0800

    coupon sql insert

--- setupDB/Coupon.sql
+++ setupDB/Coupon.sql
@@ -30,3 +30,9 @@ CREATE TABLE `Rollin`.`Coupon` (
 --
 -- Dumping data for table `Coupon`
 --
+
+insert into `Coupon` (CouponTypeID, CouponName, CouponCode, Quantity, Price, PriceCondition, StartDate, EndDate, ExpEndDate) values(1,'228','rollin228',100, 100, 500, '2020-02-27 03:00:00', '2020-02-28 00:00:00','2020-06-01 00:00:00');
+insert into `Coupon` (CouponTypeID, CouponName, CouponCode, Quantity, Price, PriceCondition, StartDate, EndDate, ExpEndDate) values(2,'birth','happyhbd',2147483647, 100, 1000, '2020-01-01 01:00:00', '2022-01-01 00:00:00','9999-01-01 01:01:00');
+insert into `Coupon` (CouponTypeID, CouponName, CouponCode, Quantity, Price, PriceCondition, StartDate, EndDate, ExpEndDate) values(3,'308','rollin38',2147483647, 0.38, 1000, '2020-03-08 00:00:00', '2020-03-11 00:00:00','2020-03-11 00:00:00');
+insert into `Coupon` (CouponTypeID, CouponName, CouponCode, Quantity, Price, PriceCondition, StartDate, EndDate, ExpEndDate) values(1,'mother','mother555',555, 0.55, 500, '2020-05-01 00:00:00', '2020-05-15 00:00:00','2020-06-30 00:00:00');
+

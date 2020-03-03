fe14db1bcd2e902b0886ba606f46f9aeb7e3debe
王韋竣 <leot7495@gmail.com>
2020-03-03 10:10:47 +0800

    setupDB

--- setupDB/News.sql
+++ setupDB/News.sql
@@ -21,3 +21,15 @@ CREATE TABLE `News`(
    photo blob,
    PRIMARY KEY ( NewsID )
 );
+
+
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title21', 'Description21', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title22', 'Description22', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title23', 'Description23', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title24', 'Description24', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title25', 'Description25', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title26', 'Description26', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title27', 'Description27', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title28', 'Description28', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title29', 'Description29', '2020-03-10', '2020-03-25', NULL);
+INSERT INTO `news` (`NewsID`, `Title`, `Description`, `CreateDate`, `UpdateDate`, `photo`) VALUES (NULL, 'Title30', 'Description30', '2020-03-10', '2020-03-25', NULL);

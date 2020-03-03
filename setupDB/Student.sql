9b2919cc5551236e4062e318d01f866b7c0e6236
PorterBD <porterbridgesds+github@gmail.com>
2020-03-03 13:34:23 +0800

    update all sql

--- setupDB/Student.sql
+++ setupDB/Student.sql
@@ -7,14 +7,18 @@ CREATE DATABASE IF NOT EXISTS Rollin DEFAULT character set utf8;
 use Rollin;
 
 --
--- Table structure for table `News`
+-- Table structure for table `Student`
 --
 
 DROP TABLE IF EXISTS `Student`;
 
 CREATE TABLE `Student`(
    StudentID  	INT     NOT NULL  AUTO_INCREMENT,
-   CourseID INT,
-   UserID int,
+   CourseID    INT,
+   UserID      int,
    PRIMARY KEY ( StudentID )
 );
+
+--
+-- Dumping data for table `Student`
+--
--- setupDB/color.sql
+++ setupDB/color.sql
@@ -12,19 +12,35 @@ use Rollin;
 -- Table structure for table `color`
 --
 
-DROP TABLE IF EXISTS `color`;
+DROP TABLE IF EXISTS `Color`;
 
-CREATE TABLE `rollin`.`color` ( 
-    `ColorID` INT(10) NOT NULL AUTO_INCREMENT , 
-    `Color` VARCHAR(20) NOT NULL , 
+CREATE TABLE  `Color` ( 
+    ColorID     INT             NOT NULL        AUTO_INCREMENT, 
+    Color       VARCHAR(20)     NOT NULL, 
     PRIMARY KEY (`ColorID`)
-    ) ENGINE = InnoDB;
-
-INSERT INTO `color` (`ColorID`, `Color`) VALUES (NULL, 'Black'), (NULL, 'White'), (NULL, 'Grey'), 
-    (NULL, 'Red'), (NULL, 'Green'), (NULL, 'Blue'), (NULL, 'Yellow'), (NULL, 'Brown'), (NULL, 'Pink'), 
-    (NULL, 'Orange'), (NULL, 'Purple'), (NULL, 'Burgundy'), (NULL, 'Tan'), (NULL, 'Gold'), (NULL, 'Camo'), 
-    (NULL, 'Tie-Dye');
+    );
 
 --
 -- Dumping data for table `color`
 --
+
+LOCK TABLES `Color` WRITE;
+
+INSERT INTO Color (Color) VALUES ('Black');
+INSERT INTO Color (Color) VALUES ('White');
+INSERT INTO Color (Color) VALUES ('Grey'); 
+INSERT INTO Color (Color) VALUES ('Red'); 
+INSERT INTO Color (Color) VALUES ('Green'); 
+INSERT INTO Color (Color) VALUES ('Blue'); 
+INSERT INTO Color (Color) VALUES ('Yellow'); 
+INSERT INTO Color (Color) VALUES ('Brown'); 
+INSERT INTO Color (Color) VALUES ('Pink'); 
+INSERT INTO Color (Color) VALUES ('Orange');
+INSERT INTO Color (Color) VALUES ('Purple'); 
+INSERT INTO Color (Color) VALUES ('Burgundy'); 
+INSERT INTO Color (Color) VALUES ('Tan'); 
+INSERT INTO Color (Color) VALUES ('Gold');
+INSERT INTO Color (Color) VALUES ('Camo'); 
+INSERT INTO Color (Color) VALUES ('Tie-Dye');
+
+UNLOCK TABLES;

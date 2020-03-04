9b921e286db1952b458024730e5d50c101df3b18
Angin-taiwan <Angin-taiwan@users.noreply.github.com>
2020-03-04 10:17:54 +0800

    0304_1017 AGC
    更正BUG (前一檔案開不了

--- setupDB/ProductStock.sql
+++ setupDB/ProductStock.sql
@@ -16,8 +16,8 @@ DROP TABLE IF EXISTS `ProductStock`;
 
 CREATE TABLE `rollin`.`ProductStock` ( 
   `ProductID`       INT(10)    NOT NULL, 
-  `SizeID`          INT(10)    NOT NULL, 
-  `ColorID`         INT(10)    NOT NULL, 
+  `SizeID`          INT(10)    NOT NULL     DEFAULT 0, 
+  `ColorID`         INT(10)    NOT NULL     DEFAULT 0, 
   `UnitInStock`     INT(10)    NOT NULL     DEFAULT 0, 
   `UnitsOnOrder`    INT(10)    NOT NULL     DEFAULT 0, 
   PRIMARY KEY (`ProductID`, `SizeID`, `ColorID`)

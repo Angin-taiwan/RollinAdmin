9f01e8c8d8fddab98987ea37ecd4b515e802aab8
Yian Hsu <yianyian2102@gmail.com>
2020-03-04 22:56:49 +0800

    user DB update, user list -pagesize

--- views/User/userList.php
+++ views/User/userList.php
@@ -6,21 +6,48 @@ $pageTitle = "User List";
 $pageDirTW = "會員管理";
 $pageTitleTW = "會員清單";
 
-$users = $data->getAll();
-$sumRow = $data->getAllCount();
+
+//分頁 下一頁
+parse_str($_SERVER['QUERY_STRING'],$query);
+$pageSize = isset($query["pageSize"]) ? $query["pageSize"] : 20;
+$pageNo = isset($query["pageNo"]) ? $query["pageNo"] : 2;
+var_dump($pageSize);
 
 
+//顯示資料筆數、分頁
+$sumRow = get_object_vars($data->getAllCount())["Total"];
+$pageTotal = intval($sumRow/$pageSize)+1; 
+$startIndex = ($pageNo - 1) * $pageSize ;
+$users = $data->getAll($startIndex,$pageSize);
+//var_dump($pageTotal);
+
+
+
+
+
+//搜尋
 if(isset($_POST["submit"])){
   $search = $_POST["search"];
   $column = $_POST["column"];
-  $users = $data->getAllLike("$column","$search");
-  $sumRow = $data->getAllLikeCount("$column","$search");
+  $startIndex = "0";
+  $_GET["pageSize"] == "" ? $pageSize ="8" : $pageSize = $_GET["pageSize"];
+  $users = $data->getAllLike($column,$search,$startIndex,$pageSize);
+  $sumRow = get_object_vars($data->getAllLikeCount($column,$search))["Total"];
 };
 
+//全部會員一覽
 if(isset($_POST["showAll"])){
-  $users = $data->getAll();
-  $sumRow = $data->getAllCount();
+  $pageSize = 20 ;
+  $users = $data->getAll($startIndex,$pageSize);
+  $sumRow = get_object_vars($data->getAllCount())["Total"];
 }
+
+
+
+
+
+// $startIndex
+// $pageSize
 
 
 require_once 'views/template/header.php';
@@ -34,6 +61,8 @@ require_once 'views/template/header.php';
       <form method="post">
         <button type="submit" name="showAll" class="btn btn-outline-primary btn-sm float-right mr-1">全部會員一覽</button>
       </form>
+      <br>
+      <!-- 搜尋 -->
       <form method="post" class="form-inline mx-auto mt-2 ml-2 align-items-center">
         <label>搜尋：</label>
         <select name="column" class="form-control form-control-sm ml-1">
@@ -45,11 +74,28 @@ require_once 'views/template/header.php';
         <button type="submit" name="submit" class="btn btn-sm btn-info ml-2">送出</button>
       </form>
       <br>
-
-      <!-- <span class="float-right">每頁顯示?筆資料，共?頁</span> -->
     </div>
     <div class="card-body">
-      <div class="text-right mr-2 mb-3 font-weight-bolder">總共 <?=$sumRow->Total?> 筆資料 </div>
+      <div class="d-flex align-items-center mb-2">
+        <form method="get" name="form" class="form-inline mr-auto">
+          <span>每頁顯示資料筆數：</span>
+          <select name="pageSize" onchange="submit()" class="form-control form-control-sm">
+            <option value="3" <?= ($pageSize == "3") ? "selected=selected":""; ?>>3</option>
+            <option value="8" <?= ($pageSize == "8") ? "selected=selected":""; ?>>8</option>
+            <option value="20" <?= ($pageSize == "20") ? "selected=selecter":""; ?>>20</option>
+          </select>
+        </form>
+        <div class="font-weight-bolder ml-auto">總共 <?=$sumRow?> 筆資料 </div>
+      </div>
+      <div>
+      <form method="get">
+        <button type="submit" name="prePage" class="btn btn-light text-dark">上一頁</button>
+        <span>第<?= $pageNo?>頁</span>
+        <button type="submit" name="nextPage" class="btn btn-light text-dark">下一頁</button>
+        <span>總共<?= $pageTotal?>頁</span>
+      </form>
+      </div>
+
       <table class="table table-bordered table-hover">
         <thead class="table-info">
           <tr>

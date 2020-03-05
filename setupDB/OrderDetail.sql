68c304b06b7aef2d17d4161060397bea258a9f89
Porter <PorterBridgesDS+github@gmail.com>
2020-03-06 01:55:14 +0800

    update sql

--- setupDB/OrderDetail.sql
+++ setupDB/OrderDetail.sql
@@ -12,14 +12,14 @@ use Rollin;
 
 DROP TABLE IF EXISTS `OrderDetail`;
 
-CREATE TABLE `rollin`.`OrderDetail` ( 
-   `OrderID`            INT      NOT NULL , 
-   `ProductID`          INT      NOT NULL , 
-   `SizeID`             INT      NOT NULL , 
-   `ColorID`            INT      NOT NULL , 
-   `DiscountGroupID`    INT      NOT NULL , 
-   `UnitPrice`          INT      NOT NULL , 
-   `Quantity`           INT      NOT NULL , 
+CREATE TABLE `rollin`.`OrderDetail` (
+   `OrderID`            INT      NOT NULL ,
+   `ProductID`          INT      NOT NULL ,
+   `SizeID`             INT      NOT NULL ,
+   `ColorID`            INT      NOT NULL ,
+   `DiscountGroupID`    INT      NOT NULL ,
+   `UnitPrice`          INT      NOT NULL ,
+   `Quantity`           INT      NOT NULL ,
    PRIMARY KEY (`OrderID`, `ProductID`, `SizeID`, `ColorID`)
 );
 
--- setupDB/size.sql
+++ setupDB/size.sql
@@ -14,9 +14,9 @@ use Rollin;
 
 DROP TABLE IF EXISTS `Size`;
 
-CREATE TABLE `rollin`.`Size` ( 
+CREATE TABLE `rollin`.`Size` (
   `SizeID`          INT            NOT NULL      AUTO_INCREMENT,
-  `SizeName`        VARCHAR(20)    NOT NULL, 
+  `SizeName`        VARCHAR(20)    NOT NULL,
   PRIMARY KEY (`SizeID`)
   );
 
@@ -49,26 +49,26 @@ LOCK TABLES `Size` WRITE;
     INSERT INTO `Size` (`SizeName`) VALUES ('9.5');
     INSERT INTO `Size` (`SizeName`) VALUES ('10');
     INSERT INTO `Size` (`SizeName`) VALUES ('10.25');
-    
+
  -- 鞋子 CategoryID = '2' ; US 7- ~ US 13+
     INSERT INTO `Size` (`SizeName`) VALUES ('US 7-');
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 7'); 
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 7.5'); 
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 8'); 
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 8.5'); 
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 7');
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 7.5');
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 8');
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 8.5');
     INSERT INTO `Size` (`SizeName`) VALUES ('US 9');
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 9.5'); 
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 9.5');
     INSERT INTO `Size` (`SizeName`) VALUES ('US 10');
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 10.5'); 
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 10.5');
     INSERT INTO `Size` (`SizeName`) VALUES ('US 11');
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 11.5'); 
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 12'); 
-    INSERT INTO `Size` (`SizeName`) VALUES ('US 12.5'); 
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 11.5');
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 12');
+    INSERT INTO `Size` (`SizeName`) VALUES ('US 12.5');
     INSERT INTO `Size` (`SizeName`) VALUES ('US 13');
     INSERT INTO `Size` (`SizeName`) VALUES ('US 13+');
-         
+
 -- 衣服CategoryID = '3'
-    -- 上身 xxS ~ xxL 
+    -- 上身 xxS ~ xxL
         INSERT INTO `Size` (`SizeName`) VALUES ('XXS');
         INSERT INTO `Size` (`SizeName`) VALUES ('XS');
         INSERT INTO `Size` (`SizeName`) VALUES ('S');
@@ -77,19 +77,19 @@ LOCK TABLES `Size` WRITE;
         INSERT INTO `Size` (`SizeName`) VALUES ('XL');
         INSERT INTO `Size` (`SizeName`) VALUES ('XXL');
     -- 褲子 W24 ~ W38
-        INSERT INTO `Size` (`SizeName`) VALUES ('W24'); 
+        INSERT INTO `Size` (`SizeName`) VALUES ('W24');
         INSERT INTO `Size` (`SizeName`) VALUES ('W25');
         INSERT INTO `Size` (`SizeName`) VALUES ('W26');
-        INSERT INTO `Size` (`SizeName`) VALUES ('W27'); 
-        INSERT INTO `Size` (`SizeName`) VALUES ('W28'); 
-        INSERT INTO `Size` (`SizeName`) VALUES ('W29'); 
-        INSERT INTO `Size` (`SizeName`) VALUES ('W30'); 
-        INSERT INTO `Size` (`SizeName`) VALUES ('W31'); 
+        INSERT INTO `Size` (`SizeName`) VALUES ('W27');
+        INSERT INTO `Size` (`SizeName`) VALUES ('W28');
+        INSERT INTO `Size` (`SizeName`) VALUES ('W29');
+        INSERT INTO `Size` (`SizeName`) VALUES ('W30');
+        INSERT INTO `Size` (`SizeName`) VALUES ('W31');
         INSERT INTO `Size` (`SizeName`) VALUES ('W32');
         INSERT INTO `Size` (`SizeName`) VALUES ('W33');
-        INSERT INTO `Size` (`SizeName`) VALUES ('W34'); 
+        INSERT INTO `Size` (`SizeName`) VALUES ('W34');
         INSERT INTO `Size` (`SizeName`) VALUES ('W35');
-        INSERT INTO `Size` (`SizeName`) VALUES ('W36'); 
+        INSERT INTO `Size` (`SizeName`) VALUES ('W36');
         INSERT INTO `Size` (`SizeName`) VALUES ('W37');
         INSERT INTO `Size` (`SizeName`) VALUES ('W38');
 

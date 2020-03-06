c962e96530fef2aa1f848e103af557c357d61b7b
Angin-taiwan <Angin-taiwan@users.noreply.github.com>
2020-03-06 15:44:51 +0800

    03061543 By AGC
    今日進度:
    1.增加Create商品庫存(多項新增進行中)
    2.篩選功能

--- views/Product/productList.php
+++ views/Product/productList.php
@@ -1,40 +1,57 @@
 <?php
 
+// 對應 header template nav active
 $pageDir = "Product";
 $pageTitle = "Product List";
 
-require_once 'views/template/header.php';
-
+// breadcrumb 中文化
+$pageDirTW = "商品管理";
+$pageTitleTW = "商品清單";
 
 
 // get set querystring
 parse_str($_SERVER['QUERY_STRING'], $query);
-$pageSize = isset($query["pageSize"]) ? $query["pageSize"] : 20;
-$ProductName =  isset($query["ProductName"]) ? $query["ProductName"] : "";
-$pageNo = isset($query["pageNo"]) ? $query["pageNo"] : 1;
 
-$array_colomns = [
-  "ProductID" => "品牌圖片",
-  "ProductName" => "品牌名稱",
-  "Description" => "品牌描述",               
+// set variables
+$pageSize = isset($query["pageSize"]) ? $query["pageSize"] : 20;
+$ProductName = isset($query["ProductName"]) ? $query["ProductName"] : "";
+$pageNo = isset($query["pageNo"]) ? $query["pageNo"] : 1;
+$show = isset($query["show"]) ? $query["show"] : "l";
+
+// set table columns
+$array_columns = [
+  "p.ProductID" => "編<br>號",
+  "p.ProductName" => "品名",
+  "b.BrandName" => "品牌",
+  "c.CategoryName" => "類別",
+  "PDescription" => "詳細",
+  "TotalStock" => "總庫存",
+  "StockOnOrder" => "訂單量",
+  "UnitPrice" => "單價",
+  "Date" => "更新日期",
 ];
 
-$columns = array_keys($array_colomns);
+// get column, sort
+$columns = array_keys($array_columns);
 $column = isset($_GET['column']) && in_array($_GET['column'], $columns) ? $_GET['column'] : $columns[0];
 $sort = isset($_GET['sort']) && strtolower($_GET['sort']) == 'desc' ? 'DESC' : 'ASC';
 
-$hidInputs = array( 
-  'column' => $column, 
-  'sort' => $sort
-); 
+// set hidden inputs
+$hidInputs = [
+  'column' => $column,
+  'sort' => $sort,
+  'show' => $show
+];
 
-$array_pageSize = array(
-  3, 6, 20
-);
+// set page size select potions
+$array_pageSize = [3, 6, 20];
 
 // for sorting
 $up_or_down = str_replace(array('ASC','DESC'), array('up','down'), $sort); 
 $asc_or_desc = $sort == 'ASC' ? 'desc' : 'asc';
+
+// for view
+$viewQuery = "Product/List?pageSize=$pageSize&ProductName=$ProductName&column=$column&sort=$sort&pageNo=$pageNo";
 
 // for pagination
 $pageStartIndex = ($pageNo - 1) * $pageSize;
@@ -42,39 +59,75 @@ $ProductsTotal = $data->getAllCount()->Total;
 $ProductsCount = $ProductName == "" ? $ProductsTotal : $data->getAllLikeCount($ProductName)->Count;
 $pagesCount = ceil((int) $ProductsCount / (int) $pageSize);
 
-#拿資料________________________________
+
+#read data_拿資料________________________________
 // $products = $data->getAll();
 $products = $data->getAllLike($ProductName, $column, $sort, $pageStartIndex, $pageSize);
 #______________________________________
+
+function createPagination($pagesCount, $pageNo, $query) {
+  if ($pagesCount > 1) {
+    $pageQuery = $query;
+    unset($pageQuery['url']);
+
+    $prevousNo = $pageNo - 1;
+    $pageQuery['pageNo'] = $prevousNo;
+    $prevousQueryString = http_build_query($pageQuery, '', '&');
+    $prevousDisabled = $prevousNo <= 0 ? "disabled" : "";
+
+    $nextNo =  $pageNo + 1;
+    $pageQuery['pageNo'] = $nextNo;
+    $nextQueryString = http_build_query($pageQuery, '', '&');
+    $nextDisabled = $nextNo > $pagesCount ? "disabled" : "";
+
+    echo "<nav aria-label='Page navigation'>";
+    echo "<ul class='pagination'>";
+    echo "<li class='page-item $prevousDisabled'><a class='page-link' href='./Product/List?$prevousQueryString'>上一頁</a></li>";
+
+    for ($i = 1; $i <= $pagesCount; $i++) {
+      $pageQuery['pageNo'] = $i;
+      $queryString = http_build_query($pageQuery, '', '&');
+      $active = ($pageNo == $i) ? "active" : "";
+      echo "<li class='page-item $active'><a class='page-link' href='./Product/List?$queryString'>$i</a></li>";
+    }
+
+    echo "<li class='page-item $nextDisabled'><a class='page-link' href='./Product/List?$nextQueryString'>下一頁</a></li>";
+    echo "</ul>";
+    echo "</nav>";
+  }
+}
+# ----------------------------------------------------------
+require_once 'views/template/header.php';
+
 ?>
-
-
-
-<script>
-// 點擊顯示詳細庫存
-function displayMore(TagName){
- var obj = document.getElementsByName(TagName);
- for(var item in obj){
-    if(obj[item].style.display==""){
-    obj[item].style.display = "none";
-    }else{
-    obj[item].style.display = "";
-    }
- };
+<style>
+th>a {
+  text-decoration:none;
+  color:#000;
 }
-// checkbox點擊全選或取消
-function checkbox() {
-  var selectallcheckbox = document.getElementById("selectallcheckbox").checked;
-  var obj = document.getElementsByName("checkbox");
-    for (var i = 0; i < obj.length; i++) {
-      if (selectallcheckbox == true){
-        obj[i].checked = true;
-      } else{
-        obj[i].checked = false;
-      }
-    }
+
+#ProductID{
+  padding:3px;
+  width:3%;
 }
-</script>
+
+#ProductName{
+  width:15%;
+}
+
+/* .td-w {
+  width: 170px;
+}
+.list-image {
+  width: 150px;
+  height: 150px;
+}
+.img-thumbnail-wrap {
+  width: 150px;
+  height: 150px;
+  margin: 1px; */
+}
+</style>
 
 
 <div class="container-fluid">
@@ -88,13 +141,11 @@ function checkbox() {
               <label for="pageSize" class="col-6 col-form-label">每頁顯示筆數</label>
               <div class="col-6">
                 <select name="pageSize" class="form-control" onchange="this.form.submit()">
-                <option value='10' $selected >10</option>
-                <option value='20' $selected >20</option>
                   <?php
-                  // foreach ($array_pageSize as $thisSize) {
-                  //   $selected = $pageSize == $thisSize ? "selected" : "";
-                  //   echo "<option value='$thisSize' $selected >$thisSize</option>";
-                  // }
+                  foreach ($array_pageSize as $thisSize) {
+                    $selected = $pageSize == $thisSize ? "selected" : "";
+                    echo "<option value='$thisSize' $selected>$thisSize</option>";
+                  }
                   ?>
                 </select>
               </div>
@@ -105,11 +156,11 @@ function checkbox() {
               <?= "總共有：$ProductsTotal 項商品" ?> 
             </label>
           </div>
-          <div class="col-3">
-            <input type="text" name="ProductName" class="form-control float-right" placeholder="輸入品牌名稱搜尋" value="<?= $ProductName ?>"  >
+          <div class="col-2">
+            <input type="text" name="ProductName" class="form-control float-right" placeholder="輸入商品名稱搜尋" value="<?= $ProductName ?>"  onchange="this.form.submit()">
 
           </div>
-          <div class="col-3">
+          <div class="col-2">
           <input type="submit" class="btn btn-dark" value="搜尋" name="searchButton">
             <label class="col-form-label" style="display: ;">
               <?= "搜尋到：$ProductsCount 個項目" ?> 
@@ -117,9 +168,9 @@ function checkbox() {
           </div>
         </div>
         <?php 
-          // foreach ($hidInputs as $k => $v) {
-          //   echo "<input type='hidden' name='$k' value='$v'>";
-          // }
+          foreach ($hidInputs as $k => $v) {
+            echo "<input type='hidden' name='$k' value='$v'>";
+          }
         ?>
         
     </form>
@@ -127,14 +178,25 @@ function checkbox() {
     </div>
     <!-- /.card-header -->
     <div class="card-body">
+    <?php createPagination($pagesCount, $pageNo, $query); ?>
     <div class=""> <!-- button -->
         <input type="button" class="btn btn-outline-info" id="checkedRevBtn" value="反選">
         <input type="submit" class="btn btn-outline-danger" id="checkedDeleteBtn" name="checkedDeleteBtn" value="勾選下架" onclick="return confirm('是否確認刪除勾選資料')">
       </div>
       <table id="listTable" class="table table-bordered table-hover">
         <thead>
-          <tr>              
-            <th><input type="checkbox" id="selectallcheckbox" onclick="checkbox()"></th>
+          <tr>
+          <th><input type="checkbox" id="selectallcheckbox" onclick="SALLcheckbox()"></th>
+          <?php
+                foreach($array_columns as $col => $colTW) {
+                  $queryString = "pageSize=$pageSize&ProductName=$ProductName&column=$col&sort=$asc_or_desc&pageNo=$pageNo";
+                  $sortClass =$column == $col ? "-" . $up_or_down : "";
+                  echo "<th id='$col'>";
+                  echo "<a href='/RollinAdmin/Product/List?$queryString'>$colTW<i class='fas fa-sort$sortClass'></i></a>";
+                  echo "</th>";
+                }
+                ?>            
+<!-- 
             <th style='width:2%'>ID</th>
             <th style='width:15%'>Name</th>
             <th>Brand</th>
@@ -143,7 +205,7 @@ function checkbox() {
             <th style='width:7%'>總庫存</th>
             <th style='width:7%'>訂單量</th>
             <th>單價</th>
-            <th>更新日期</th> <!-- Date -->
+            <th>更新日期</th> Date -->
           </tr>
         </thead>
         <tbody>
@@ -169,13 +231,17 @@ function checkbox() {
             //點擊顯示下拉庫存
                   $Stocks = $data->getStock($pd->ProductID);
                   foreach ($Stocks as $pdst) {
+                    $SizeName ="One Size" ;
+                    if(isset($pdst->SizeName)){
+                      $SizeName = "$pdst->SizeName";
+                    }
                     echo  <<<here
                     <tr name='$pd->ProductID' style='display: none;' >
                     <td bgcolor="#778899"></td>
                     <td bgcolor="#778899"></td>
                     <td bgcolor="#778899"></td>
-                    <td bgcolor="#778899">Size:$pdst->SizeID </td>
-                    <td bgcolor="#778899">Color:$pdst->ColorID </td>
+                    <td bgcolor="#778899">Size:$SizeName </td>
+                    <td bgcolor="#778899">Color:$pdst->Color </td>
                     <td bgcolor="#778899"></td>
                     <td bgcolor="#778899">pdst:$pdst->UnitInStock</td>
                     <td bgcolor="#778899"></td>
@@ -204,3 +270,40 @@ function checkbox() {
 require_once 'views/template/footer.php';
 
 ?>
+<script>
+// 點擊顯示詳細庫存
+function displayMore(TagName){
+ var obj = document.getElementsByName(TagName);
+ for(var item in obj){
+    if(obj[item].style.display==""){
+    obj[item].style.display = "none";
+    }else{
+    obj[item].style.display = "";
+    }
+ };
+}
+var obj = document.getElementsByName("checkbox");
+// checkbox點擊全選或取消
+function SALLcheckbox() {
+  var selectallcheckbox = document.getElementById("selectallcheckbox").checked;
+    for (var i = 0; i < obj.length; i++) {
+      if (selectallcheckbox == true){
+        obj[i].checked = true;
+      } else{
+        obj[i].checked = false;
+      }
+    }
+}
+//反選按鈕
+
+let checkedRevBtn = document.getElementById("checkedRevBtn");
+    checkedRevBtn.onclick = function() {
+      for (var j = 0; j < obj.length; j++) {
+        if (obj[j].checked) {
+          obj[j].checked = false;
+        } else {
+          obj[j].checked = true;
+        }
+      }
+    }
+</script>
